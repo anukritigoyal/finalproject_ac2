3847ffde7744c113932f98e4a6c764ffeaa87d5c
Anukriti Goyal <Nikki@D-173-250-206-107.dhcp4.washington.edu>
2017-05-28 18:03:47 -0700

    updated BuildMap

--- scripts/buildMap.R
+++ scripts/buildMap.R
@@ -1,39 +1,35 @@
-library(plotly)
-library(dplyr)
+#library(plotly)
+#library(dplyr)
 
-BuildMap <- function(data, data.year) {
+BuildMap <- function(data, data.year, type) {
+  data <- data %>% select(YearStart, LocationAbbr, LocationDesc, Topic, DataValue,
+                          DataValueType, DataValueUnit, StratificationCategory1, Stratification1)
   
-}
-
-diabetes <- diabetes.data %>% select(YearStart, LocationAbbr, LocationDesc, Topic, DataValue,
-                                     DataValueType, DataValueUnit,
-                                     StratificationCategory1, Stratification1)
-diabetes <- diabetes %>% filter(YearStart == "2010") %>% filter(StratificationCategory1 == "Overall") %>%
-  filter(DataValueType == "Number")
-
-diabetes <- diabetes %>% group_by(LocationAbbr) %>% summarise(DataValue = max(DataValue, na.rm = TRUE))
-
-
-
-g <- list (
-  scope = 'usa',
-  projection = list(type = 'albers usa'),
-  showland = TRUE,
-  landcolor = toRGB("gray85"),
-  subunitwidth = 1,
-  countrywidth = 1,
-  subunitcolor = toRGB("white"),
-  countrycolor = toRGB("white")
-)
-
-map <- plot_geo(diabetes, locationmode = 'USA-states') %>%
-  add_trace(
-    z = ~DataValue, color = ~DataValue,
-    locations = ~LocationAbbr, colors = 'Blues'
-  ) %>%
-  colorbar(title = "cases per 100,000 people") %>%
-  layout(
-    title = 'Mortality Rates of <enter disease here> in <enter year here>',
-    geo = g
+  data <- data %>% filter(YearStart == data.year) %>% filter(StratificationCategory1 == "Overall") %>%
+    filter(DataValueType == "Number") %>% filter(LocationAbbr != "US")
+  
+  data <- data %>% group_by(LocationAbbr) %>% summarise(newData = sum(DataValue, na.rm = TRUE))
+  
+  g <- list (
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showland = TRUE,
+    landcolor = toRGB("gray85"),
+    subunitwidth = 1,
+    countrywidth = 1,
+    subunitcolor = toRGB("white"),
+    countrycolor = toRGB("white")
   )
+  
+  map <- plot_geo(data, locationmode = 'USA-states') %>%
+    add_trace(
+      z = ~newData, color = ~newData,
+      locations = ~LocationAbbr, colors = 'Blues'
+    ) %>%
+    colorbar(title = "cases per 100,000 people") %>%
+    layout(
+      title = 'Mortality Rates in the United States',
+      geo = g
+    )
+}
 
