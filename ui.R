b8d63e1418bda692a04bb0e48c990a3ebe2c640d
Anukriti Goyal <Nikki@D-69-91-232-34.dhcp4.washington.edu>
2017-05-31 16:39:38 -0700

    written project description

--- ui.R
+++ ui.R
@@ -40,9 +40,23 @@ shinyUI(navbarPage("Chronic Diseases in the US",
                h1("Project Documentation"),
                h5("Created by: Anukriti Goyal, Kristine Ly, Tina Zhuang & Simin Huan"),
                br(),
-               h2("Project Description")
+               h2("Project Description"),
+               h3("Purpose"),
+               p("The purpose of our project was to depict the relationship between the existence of 
+                 of chronic diseases and the different states that they exist in as well as the 
+                 different groups of people the diseases affect annually. We chose to focus on 
+                 Diabetes, Chronic Obstructive Pulmonary Disease and Cardiovascular Disease
+                 as they lead to the most mortalities annually in the United States."),
+               br(),
+               h3("Data Source"),
+               p("The data set that we worked with can be found at: https://catalog.data.gov/dataset/u-s-chronic-disease-indicators-cdi-e50c9"),
+               p("The information was found in the Health section of Data.gov, however, was initially
+                 published by the Centers for Disease Control and Prevention")
                
              )
     )
     
 ))
+
+
+# The purpose of the project, source of the data, and any pertinent information about the topic area are introduced. You may also want to include links to the code, or information about the team.
