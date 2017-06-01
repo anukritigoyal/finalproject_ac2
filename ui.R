0280cfed637b57dc6ed4fb9a3981e11574bc8c4b
Siminnn <huans@uw.edu>
2017-05-31 17:09:49 -0700

    doc

--- ui.R
+++ ui.R
@@ -36,28 +36,14 @@ shinyUI(navbarPage("Chronic Diseases in the US",
              )
     ),
     tabPanel('Documentation',
-             mainPanel(
-               h1("Project Documentation", style = "font-family: 'trebuchet ms';"),
-               h5("Created by: Anukriti Goyal, Kristine Ly, Tina Zhuang & Simin Huan", style = "font-family: 'trebuchet ms';"),
-               br(),
-               h2("Project Description", style = "font-family: 'trebuchet ms';"),
-               h3("Purpose", style = "font-family: 'trebuchet ms';"),
-               p("The purpose of our project was to depict the relationship between the existence of 
-                 of chronic diseases and the different states that they exist in, as well as the 
-                 different groups of people the diseases affect annually. We chose to focus on 
-                 Diabetes, Chronic Obstructive Pulmonary Disease and Cardiovascular Disease
-                 as they had the most annual indicators of mortality in the United States. We were 
-                 interested in seeing the change in the mortality rate as developments in 
-                 healthcare advanced over the years and health officials continuous actions
-                 lead to improvements in healthcare services ", style = "font-family: 'trebuchet ms';"),
-               br(),
-               h3("Data Source", style = "font-family: 'trebuchet ms';"),
-               p("The data set that we worked with can be found at: https://catalog.data.gov/dataset/u-s-chronic-disease-indicators-cdi-e50c9",
-                 style = "font-family: 'trebuchet ms';"),
-               p("The information was found in the Health section of Data.gov, however, it was initially
-                 published by the Centers for Disease Control and Prevention", style = "font-family: 'trebuchet ms';")
-             )
-    )
+
+
+                 includeMarkdown("document.Rmd")
+               )
+             
+    
+
+
     
 ))
 
